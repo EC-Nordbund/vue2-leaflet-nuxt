cec089880fc9cd564456913b429e2902eed673b3
Sebastian Krüger <2pi_r2@gmx.de>
2021-02-03 14:42:54 +0100

    fixes, v3.0

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import * as fs from 'fs'
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 const genEntry = fs.readdirSync('./src/components').map(v => [v.split('.')[0], `./src/components/${v}`])
 
@@ -11,7 +12,6 @@ genEntry.forEach(v => {
   entry[v[0]] = v[1]
 })
 
-// console.log(entry)
 
 export default {
   output: {
@@ -20,12 +20,24 @@ export default {
   },
   input: entry,
   plugins: [
+    {
+      name: 'leaflet',
+      resolveId(id) {
+        if (id === 'leaflet') {
+          return '@ec-nordbund/leaflet'
+        }
+      }
+    },
+    nodeResolve(),
     json(),
     commonjs({
       include: './src/components/*.js',
       transformMixedEsModules: true
     }),
     terser()
+  ],
+  external: [
+    '@ec-nordbund/leaflet'
   ]
 }
 
--- src/build.js
+++ src/build.js
@@ -26,7 +26,7 @@ fs.readdirSync('./node_modules/vue2-leaflet/src/components').forEach(f => {
 })
 
 // Do css
-const css = [require.resolve('leaflet/dist/leaflet.css'),
+const css = [require.resolve('@ec-nordbund/leaflet/dist/leaflet.css'),
 require.resolve('./gestureHandling/leaflet-gesture-handling.css')]
 
 if (!fs.existsSync('./dist')) {
@@ -36,7 +36,7 @@ if (!fs.existsSync('./dist')) {
 const cssCode = css.map(v => fs.readFileSync(v, 'utf-8')).join('\n')
 fs.writeFileSync('./dist/style.css', cssCode)
 
-const base = path.join(path.dirname(require.resolve('leaflet')), '..', 'dist', 'images')
+const base = path.join(path.dirname(require.resolve('@ec-nordbund/leaflet')), '..', 'dist', 'images')
 const t = path.join(__dirname, '..', 'dist', 'images')
 
 if (!fs.existsSync('./dist/images')) {
